0b745384d07b9cfd90dc79397344053f69bd57ab
AlexV <143393196+AlexV-7@users.noreply.github.com>
2023-09-06 02:05:36 +1000

    Changes to JS

--- src/create_event.js
+++ src/create_event.js
@@ -1,29 +1,54 @@
-// import addEventToFirestore function from backend js file
-import { addEventToFirestore } from ". /backend/firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 
-document. addEventListener ("DOMContentLoaded", function () {
-    
-    // Get the form element
-    const form = document. querySelector ("form");
-    
-    // Listen for the submit event on the form
-    form. addEventListener ("submit", async function (event) {
-        
-        // Prevent the default form submission behavior
-        event. preventDefault ();
-        
-        // Create an object to hold the form data
-        const formData = {            
-            eventName: form.cename.value,
-            organiserName: form.ceorganiser.value,
-            eventCategory: form.cecategory.value,
-            eventLocation: form.celocation.value,
-            eventDate: form.cedate.value,
-            time: form.cetime.value,
-            wifi: form.cewifi.value,
-        };
-        
-        // Call the addEventToFirestore function 
-        await addEventToFirestore (formData);
-    });
-});
+// Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyC2560pETTPekmI-ABPf_xZ3M3UB6ZqMK4",
+    authDomain: "message-display-system.firebaseapp.com",
+    databaseURL: "https://message-display-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "message-display-system",
+    storageBucket: "message-display-system.appspot.com",
+    messagingSenderId: "260068152843",
+    appId: "1:260068152843:web:2336283608fddc0e41d90e",
+    measurementId: "G-HCK0JW6RYC"
+  };
+
+// Initialise Firebase and Firestore
+const app = initializeApp(firebaseConfig);
+
+// reference your database
+var eventDetailsDB = firebase.database().ref("eventDetails");
+
+document.getElementById("eventDetails").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var eventName = getElementById("cename");
+  var organiserName = getElementById("ceorganiser");
+  var eventCategory = getElementById("cecategory");
+  var eventLocation = getElementById("celocation");
+  var eventDate = getElementById("cedate");
+  var time = getElementById("cetime");
+  var wifi = getElementById("cewifi");
+
+  saveData(eventName, organiserName, eventCategory, eventLocation, eventDate, time, wifi);
+
+}
+
+const saveData = (eventName, organiserName, eventCategory, eventLocation, eventDate, time, wifi) => {
+  var neweventDetails = eventDetailsDB.push();
+
+  neweventDetails.set({
+    eventName: cename,
+    organiserName: ceorganiser,
+    eventCategory: cecategory,
+    eventLocation: celocation,
+    eventDate: cedate,
+    time: cetime,
+    wifi: cewifi,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
